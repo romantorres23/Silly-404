eaca7c2a5ff1009cb344b011f135f9706d2c8a16
Roman Torres <rtorres@envivent.com>
2022-09-06 16:32:17 -0700

    converted to a 404

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, { Suspense } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import Model from './components/Model';
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Model from "./components/Model";
 
 export default function App() {
-   return (
+  return (
+    <>
+      <div className="copy">
+        <h1>Lost in the dungeon?</h1>
+        <p>That which you seek does not exist</p>
+      </div>
       <Canvas
-         camera={{ position: [2, 0, 12.25], fov: 15 }}
-         style={{
-            backgroundColor: '#111a21',
-            width: '100vw',
-            height: '100vh',
-         }}
+        camera={{ position: [2, 0, 12.25], fov: 15 }}
+        style={{
+          backgroundColor: "#0b1217",
+          width: "100vw",
+          height: "100vh",
+        }}
       >
-         <ambientLight intensity={1.25} />
-         <ambientLight intensity={0.1} />
-         <directionalLight intensity={0.4} />
-         <Suspense fallback={null}>
-            <Model position={[0.025, -0.9, 0]} />
-         </Suspense>
-         <OrbitControls />
+        <ambientLight intensity={1.25} />
+        <ambientLight intensity={0.1} />
+        <directionalLight intensity={0} />
+        <Suspense fallback={null}>
+          <Model position={[0.025, -0.9, 0]} />
+        </Suspense>
+        <OrbitControls />
       </Canvas>
-   );
+    </>
+  );
 }
